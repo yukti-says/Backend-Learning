958d98608ebdf18c11b2bcf6b8f9fedc1c6f90db
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-06-16 18:57:48 +0530

    created User schema and info

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -5,3 +5,43 @@ mongoose.connect(
 )
     .then(() => console.log("connected"))
     .catch(e=>console.log("not connected"))
+
+
+// create user schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createAt: {
+    type: Date,
+    default:Date.now
+  }
+})
+   
+// usermodel
+const User = mongoose.model('User', userSchema)
+
+async function runQuery() {
+  try {
+    // create new user aka document
+    const newuser = await User.create({
+      name: "yukti",
+      email: "psjfr@gmail.com",
+      age: 20,
+      isActive: true,
+      tags:['coder','cooker','cooler']
+    })
+
+    console.log("created user",newuser)
+  }
+  catch (e) {
+    console.log('error ->',e)
+  }
+  finally {
+    await mongoose.connection.close()
+  }
+}
+
+runQuery()
